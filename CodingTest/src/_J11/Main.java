1524f1a82d7436bb2265784d65821da8f5e86956
kimtaemin <xoalstjtm@gmail.com>
2021-07-07 00:23:18 +0900

    답안 추가

--- CodingTest/src/_J11/Main.java
+++ CodingTest/src/_J11/Main.java
@@ -32,7 +32,7 @@ public class Main {
 				chkCnt ++;
 			} else if(chkCnt != 1) {
 				answer += chkCnt;
-				chkCnt = 1; 						    // 다음 인덱스랑 다른 경우 --> 현재 카운트를 저장 하지만 카운트가 1이면 저장 안 함.
+				chkCnt = 1; 						// 다음 인덱스랑 다른 경우 --> 현재 카운트를 저장 하지만 카운트가 1이면 저장 안 함.
 			}
 			
 			if(i == s.length()-2) {
@@ -46,10 +46,30 @@ public class Main {
 		return answer;
 	}
 	
+	// 강의 답안 
+	public static String solution(String s) {
+		String answer = "";
+		s=s+" ";
+		int cnt = 1;
+		
+		for(int i=0; i<s.length()-1; i++) {
+			
+			if(s.charAt(i) == s.charAt(i+1)) {		// 반복 출연인 경우 카운트 증가 
+				cnt ++;
+			} else {
+				answer += s.charAt(i);
+				if(cnt>1) answer += String.valueOf(cnt);
+				cnt = 1; 						    // 다음 인덱스랑 다른 경우 --> 현재 카운트를 저장 하지만 카운트가 1이면 저장 안 함.
+			}
+		}
+		return answer;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
 		System.out.println(solution2(str));
+		System.out.println(solution(str));
 	}
 	
 	

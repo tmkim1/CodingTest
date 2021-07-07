5b1c2eb0b0f08aa92c2d825fd247375f6063a7e7
kimtaemin <xoalstjtm@gmail.com>
2021-07-08 00:24:40 +0900

    [자바 알고리즘 문제 풀이] - 가위 바위 보 승자 구하기

---
+++ CodingTest/src/J_15/Main.java
@@ -0,0 +1,75 @@
+package J_15;
+
+import java.util.Scanner;
+
+/**
+ * 15. 가위 바위 보
+ * 
+ * 문제 설명: 큰 수 출력하기 
+ * 가위, 바위, 보의 정보는 1:가위, 2:바위, 3:보
+ * 
+ * [입력] 
+ * 5
+ * 2 3 3 1 3
+ * 1 1 2 2 3
+ * [출력]
+ * A
+ * B
+ * A
+ * B
+ * D
+ * 출처: 인프런 자바 알고리즘 문제풀이 
+ * @author kimtaemin
+ */
+
+public class Main {
+	public static void solution(int[] aArr, int[] bArr) {
+		// 1:가위, 2:바위, 3:보 
+		// 1 ::  상대: 2 lose, 3 win 
+		// 2 ::  상대: 3 lose, 1 win 
+		// 3 ::  상대: 1 	lose, 2 win 
+		for(int i=0; i<aArr.length; i++) {
+			if(aArr[i] == 1) {				//A가 가위를 낸 경우 
+				if(bArr[i] == 1) {
+					System.out.println("D");
+				} else if(bArr[i] == 2) {
+					System.out.println("B");
+				} else {
+					System.out.println("A");
+				}
+			} else if(aArr[i] == 2) {		//A가 바위를 낸 경우
+				if(bArr[i] == 1) {
+					System.out.println("A");
+				} else if(bArr[i] == 2) {
+					System.out.println("D");
+				} else {
+					System.out.println("B");
+				}
+			} else {                        //A가 보를 낸 경우
+				if(bArr[i] == 1) {
+					System.out.println("B");
+				} else if(bArr[i] == 2) {
+					System.out.println("A");
+				} else {
+					System.out.println("D");
+				}
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] aArr = new int[size];  
+		int[] bArr = new int[size];
+		
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<size; j++) {
+				if(i == 0) aArr[j] = sc.nextInt();
+				else bArr[j] = sc.nextInt();
+			}
+		}
+		solution(aArr, bArr);
+	}
+}

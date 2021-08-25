24635180f288d2b7d1ecd30dd32f752ce90947ac
tmkim1 <55820309+tmkim1@users.noreply.github.com>
2021-08-26 08:56:21 +0900

    Update Main.java

--- CodingTest/src/_J18/Main.java
+++ CodingTest/src/_J18/Main.java
@@ -34,7 +34,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		ArrayList<Integer> list = new ArrayList<>(); //소수를 담을 List
 		
 		for(int i=0; i<n; i++) {
 			int number = sc.nextInt();

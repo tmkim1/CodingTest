89ef8ccfa19514f7a93aa8afbfc9725cb21ee73a
tmkim1 <xoalstjtm@gmail.com>
2021-08-26 13:43:35 +0900

    에라토스테네스 체 복습 및 설명 추가

--- CodingTest/src/_J17/Main.java
+++ CodingTest/src/_J17/Main.java
@@ -1,5 +1,24 @@
 package _J17;
 
+/**
+ * 17. 소수 구하기 (에라토스테네스 체) 
+ * 
+ * 문제 설명: 자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.
+ * 
+ * 만약 20이 입력되면 1부터 20까지의 소수는 2, 3, 5, 7, 11, 13, 17, 19로 총 8개입니다
+ * 
+ * [입력] 
+ *  20	
+ * [출력]
+ * 8
+ * 
+ * 출처: 인프런 자바 알고리즘 문제풀이 
+ * 
+ * 에라토스테네스의 체 알고리즘 설명: https://firework-ham.tistory.com/8
+ * 
+ * @author kimtaemin
+ */
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -22,11 +41,11 @@ public class Main {
 		
 		boolean[] check = new boolean[number+1];	//소수 판별 true: 비소수, false: 소수 
 		check[0] = check[1] = true;				    //0과 1은 소수가 아니므로 true 
-		
-		for(int i=2; i<=Math.sqrt(number); i++) {
+		System.out.println(number + "의 제곱근: " + Math.sqrt(number));
+		for(int i=2; i<=Math.sqrt(number); i++) {   //Math.sqrt: 제곱 근 구하기 
 			if(check[i]) continue;                  //소수가 아닐 경우 skip
 			
-			for(int j=i*i; j<check.length; j += i) {
+			for(int j=i+i; j<check.length; j+=i) {
 				check[j] = true;
 			}
 		}

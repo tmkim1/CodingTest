9f31a74cba457b913d4db4671a0fcb4e0d48bca7
tmkim1 <xoalstjtm@gmail.com>
2021-08-27 11:38:47 +0900

    탭 수정

--- CodingTest/src/_J37/Main.java
+++ CodingTest/src/_J37/Main.java
@@ -23,44 +23,44 @@ import java.util.Stack;
 public class Main {
 
 	//내 풀이 
-		//'(' 괄호를 만나면 스택에 넣어놓고 스택에 괄호가 존재하면 알파벳 건너띄고 
-	    //')' 괄호를 만나면 스택에 있던 '(' 괄호를 제거한다.
-		public static String solution(String str) {
-			String answer = "";
-			Stack<Character> stack = new Stack<>();
-			
-			for(char c : str.toCharArray()) {
-				if( c == '(') stack.push(c);
-				else if( c == ')') stack.pop(); //스택에 존재하는 '(' 문자를 pop
-				else {
-					if(stack.size() == 0) answer += c;//괄호 안에 있는 문자가 아님 List에 추가  
-				}
+	//'(' 괄호를 만나면 스택에 넣어놓고 스택에 괄호가 존재하면 알파벳 건너띄고 
+    //')' 괄호를 만나면 스택에 있던 '(' 괄호를 제거한다.
+	public static String solution(String str) {
+		String answer = "";
+		Stack<Character> stack = new Stack<>();
+		
+		for(char c : str.toCharArray()) {
+			if( c == '(') stack.push(c);
+			else if( c == ')') stack.pop(); //스택에 존재하는 '(' 문자를 pop
+			else {
+				if(stack.size() == 0) answer += c;//괄호 안에 있는 문자가 아님 List에 추가  
 			}
-			
-			return answer;
 		}
 		
+		return answer;
+	}
+	
+	
+	//강의 답안 --> 스택에 넣고 빼는 방법으로 처리 
+	public static void solution2(String str) {
+		Stack<Character> stack = new Stack<>();
 		
-		//강의 답안 --> 스택에 넣고 빼는 방법으로 처리 
-		public static void solution2(String str) {
-			Stack<Character> stack = new Stack<>();
-			
-			for(char c : str.toCharArray()) {
-				if( c== ')') {					// 닫는 괄호를 만난 순간 여는 괄호를 만나는 지점까지 모두 pop하여 제거 
-					while(stack.pop() != '(');
-				} else stack.push(c);			
-			}
-			//마지막에 남는 문자들은 괄호 안에 있지 않던 문자가 됨. 
-			for(int i=0; i<stack.size(); i++) System.out.print(stack.get(i));
-			
+		for(char c : str.toCharArray()) {
+			if(c == ')') {					// 닫는 괄호를 만난 순간 여는 괄호를 만나는 지점까지 모두 pop하여 제거 
+				while(stack.pop() != '(');
+			} else stack.push(c);			
 		}
+		//마지막에 남는 문자들은 괄호 안에 있지 않던 문자가 됨. 
+		for(int i=0; i<stack.size(); i++) System.out.print(stack.get(i));
 		
-		public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			String str = sc.next();
-			solution(str);
-			System.out.println(solution(str));
-			solution2(str);
-			
-		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		solution(str);
+		System.out.println(solution(str));
+		solution2(str);
+		
+	}
 }
